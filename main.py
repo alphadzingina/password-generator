f1cd1b5d5c6ffe4ded5c2d99224befc1ae051fe8
alphadzingina <alphaadx6@gmail.com>
2022-01-22 15:11:31 +0100

    generate password

--- main.py
+++ main.py
@@ -12,3 +12,15 @@ nr_letters= int(input("How many letters would you like in your password?\n>>> ")
 nr_symbols = int(input(f"How many symbols would you like?\n>>> "))
 nr_numbers = int(input(f"How many numbers would you like?\n>>> "))
 
+password = ""
+
+for char in range(1, nr_letters + 1):
+    password = password + random.choice(letters)
+
+for char in range(1, nr_numbers + 1):
+    password =  password + random.choice(numbers)
+
+for char in range(1, nr_symbols + 1):
+    password =  password + random.choice(symbols)
+
+print(f"Your password is \'{password}\'")
